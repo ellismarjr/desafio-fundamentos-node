5fe4eb414e1d25350685841cdc41e874ec31de97
Júnior de Oliveira <ellismarjr@hotmail.com>
2020-05-06 13:00:43 -0300

    feature: Create transaction, get balance and test

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,31 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!value) {
+      throw Error('The value is not valid!');
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('The type of transaction is not valid!');
+    }
+
+    if (type === 'income' && value <= 0) {
+      throw Error('INCOME transaction. Value must be greather then ZERO');
+    }
+
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > Math.abs(balance.total)) {
+      throw Error('Transaction OUTCOME, You are not allowed to use overdraft!');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
